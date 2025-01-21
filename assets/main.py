f1bd73159ac754f21640a3445a8aa2c74824ef7c
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2025-01-21 15:39:21 -0300

    feat: settings main.py

--- assets/main.py
+++ assets/main.py
@@ -1,25 +1,28 @@
 import tiktoken
 import safetensors
 import torch
+from torch.nn import functional as F
 
-# f = open("tiny.txt", "r")
-# enc = tiktoken.encoding_for_model("gpt2")
-# tokens = enc.encode(f.read())
+f = open("tiny.txt", "r")
+enc = tiktoken.encoding_for_model("gpt2")
+tokens = enc.encode(f.read())
 # text = enc.decode(tokens)
 
-# print(text)
+x = tokens[:64]
+y_true = tokens[1:65]
 
-f = open("model.safetensors", "rb")
+print(x)
+print(y_true)
+exit(0)
+
+f = open("model.safetensors", "rb") 
 fts = safetensors.deserialize(f.read())
 params = dict()
-
 for ft in fts:
     name = ft[0]
     data = ft[1]["data"]
     shape = ft[1]["shape"]
-
     params[name] = torch.frombuffer(data, dtype=torch.float32).reshape(shape)
+# print(params["wte.weight"].shape)
 
-
-print(params["wte.weight"].shape)
-
+F.embedding(input, weight)
